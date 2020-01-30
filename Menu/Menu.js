70587493102a93db5ad0012f22fa3180c03f0d80
Petros Demetriou <dimitriupe@gmail.com>
2020-01-30 18:14:22 +0200

    Finished the Componets I project

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -36,3 +36,55 @@ let menuItems = [
 
 // Step 1
 
+function menuCreator(menuItems) {
+
+  // Step 2
+
+  const menuDiv = document.createElement('div');
+  const unorderedlist = document.createElement('ul');
+  const menuItem1 = document.createElement('li');
+  const menuItem2 = document.createElement('li');
+  const menuItem3 = document.createElement('li');
+  const menuItem4 = document.createElement('li');
+  const menuItem5 = document.createElement('li');
+  const menuItem6 = document.createElement('li');
+
+  menuDiv.appendChild(unorderedlist);
+
+  unorderedlist.appendChild(menuItem1);
+  unorderedlist.appendChild(menuItem2);
+  unorderedlist.appendChild(menuItem3);
+  unorderedlist.appendChild(menuItem4);
+  unorderedlist.appendChild(menuItem5);
+  unorderedlist.appendChild(menuItem6);
+
+  menuDiv.classList.add('menu');
+
+  // Step 3
+
+  const menuBtn = document.querySelector('.menu-button');
+
+  // Step 4
+
+  menuBtn.addEventListener('click', () => {
+    const menuDiv1 = document.querySelector('.menu');
+    menuDiv1.classList.toggle('menu--open');
+  });
+
+  menuItem1.textContent = menuItems[0];
+  menuItem2.textContent = menuItems[1];
+  menuItem3.textContent = menuItems[2];
+  menuItem4.textContent = menuItems[3];
+  menuItem5.textContent = menuItems[4];
+  menuItem6.textContent = menuItems[5];
+
+  //Step 5
+
+  return menuDiv;
+}
+
+// Step 6
+
+const head = document.querySelector('.header');
+head.appendChild(menuCreator(menuItems));
+
